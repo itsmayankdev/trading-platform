9e90762b90e2b01b2425337cc47a7933bba541af
itsmayankdev <extramailsformayank@gmail.com>
2026-09-10 09:39:02 +0530

    2nd-update-HFDS: lock registry coverage for reference matcher

--- tests/pattern_engine/test_registry.py
+++ tests/pattern_engine/test_registry.py
@@ -4,13 +4,14 @@ from pattern_engine.algorithms.v1 import SimilarityV1
 from pattern_engine.algorithms.v2 import SimilarityV2
 from pattern_engine.algorithms.v3 import SimilarityV3
 from pattern_engine.algorithms.v4 import SimilarityV4
+from pattern_engine.algorithms.v5 import SimilarityV5
 from pattern_engine.registry import PRODUCTION_ALGORITHM_VERSION, get_algorithm, list_algorithms
 
 
-def test_production_algorithm_is_strict_v4():
+def test_production_algorithm_is_reference_v5():
     algorithm = get_algorithm()
-    assert PRODUCTION_ALGORITHM_VERSION == "similarity_v4"
-    assert isinstance(algorithm, SimilarityV4)
+    assert PRODUCTION_ALGORITHM_VERSION == "similarity_v5"
+    assert isinstance(algorithm, SimilarityV5)
 
 
 def test_all_versions_are_explicitly_selectable():
@@ -18,7 +19,8 @@ def test_all_versions_are_explicitly_selectable():
     assert isinstance(get_algorithm("similarity_v2"), SimilarityV2)
     assert isinstance(get_algorithm("similarity_v3"), SimilarityV3)
     assert isinstance(get_algorithm("similarity_v4"), SimilarityV4)
-    assert set(list_algorithms()) == {"similarity_v1", "similarity_v2", "similarity_v3", "similarity_v4"}
+    assert isinstance(get_algorithm("similarity_v5"), SimilarityV5)
+    assert set(list_algorithms()) == {"similarity_v1", "similarity_v2", "similarity_v3", "similarity_v4", "similarity_v5"}
 
 
 def test_unknown_algorithm_is_rejected():
